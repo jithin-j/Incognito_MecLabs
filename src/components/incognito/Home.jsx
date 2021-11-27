a9e55860fde05d6abef8326b56d8a2fbb3b899ae
jithin-j <jithinjagadeesh1@gmail.com>
2021-11-27 12:34:23 +0530

    Changed background colour

--- src/components/incognito/Home.jsx
+++ src/components/incognito/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Navbar from './Navbar'
+import "../stylesheet/mainstyle.css"
 const Home = () => {
     return (
         <div>
+            <div className="main"></div>
            <Navbar /> 
         </div>
     )
